66c056aa360de5165b5caee716003eb208d3c492
jimmycliff obonyo <cliffjimmy27@gmail.com>
2021-02-11 05:29:13 +0300

    init

---
+++ snowpack.config.cjs
@@ -0,0 +1,22 @@
+// Consult https://www.snowpack.dev to learn about these options
+module.exports = {
+	plugins: [
+		[
+			'@snowpack/plugin-build-script',
+			{
+				cmd: "postcss",
+				input: [".css", ".pcss"],
+				output: [".css"],
+			}
+		],
+	
+		
+	],
+	extends: '@sveltejs/snowpack-config',
+	mount: {
+		'src/components': '/_components'
+	},
+	alias: {
+		$components: './src/components'
+	}
+};
