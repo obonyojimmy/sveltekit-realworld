fad341d55bd4a63bdcec73294d5c1c51c3bdc341
jimmycliff obonyo <cliffjimmy27@gmail.com>
2021-02-11 05:42:21 +0300

    add tailwindcss

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,3 +1,4 @@
+const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
 const cssnano = require("cssnano");
 
@@ -6,6 +7,12 @@ const dev = mode === "development";
 
 module.exports = {
 	plugins: [
+		// Some plugins, like postcss-nested, need to run before Tailwind
+		
+		tailwindcss,
+		
+		// But others, like autoprefixer, need to run after
+
 		autoprefixer,
 		
 		!dev && cssnano({
